be9a2336a1db99d233947483772ac7176b1cd499
bhagatz <bhagat.singh@zomato.com>
2017-12-01 19:22:08 +0530

    Initial Commit

---
+++ CoreML-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML-Demo
+//
+//  Created by Bhagat  Singh on 01/12/17.
+//  Copyright © 2017 Bhagat  Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
